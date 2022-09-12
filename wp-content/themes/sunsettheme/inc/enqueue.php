bec6e5a4a1c15d4eaea22141effdceb822ec6275
MoshiurRaihan95 <moshiurraihan95@gmail.com>
2022-09-12 17:54:25 +0600

    added js file for custom media query

--- wp-content/themes/sunsettheme/inc/enqueue.php
+++ wp-content/themes/sunsettheme/inc/enqueue.php
@@ -13,5 +13,9 @@ function sunset_load_admin_script( $hook ){
     }
     wp_register_style('sunset_admin', get_template_directory_uri() . '/assets/css/sunset.admin.css', array(),'1.0.0', 'all'); 
     wp_enqueue_style('sunset_admin');
+
+    // cunect jQuery file
+    wp_register_script('sunset_admin_script', get_template_directory_uri() . '/assets/js/sunset.admin.js', array('jQuery'), '1.0.0', true);
+    wp_enqueue_script('sunset_admin_script');
 }
 add_action('admin_enqueue_scripts', 'sunset_load_admin_script');
--- wp-content/themes/sunsettheme/inc/templates/sunset-admin.php
+++ wp-content/themes/sunsettheme/inc/templates/sunset-admin.php
@@ -5,19 +5,20 @@
     $firstName = esc_attr(get_option('first_name'));
     $lastName = esc_attr(get_option('last_name'));
     $fullName = $firstName . ' ' . $lastName;
+    $description = esc_attr(get_option('user_description'));
 ?>
 
 <div class="sunset_sidebar_preview">
     <div class="sunset_sidebar">
         <h1 class="sunset_username"><?php print $fullName; ?></h1>
-        <h2 class="sunset_description"></h2>
+        <h2 class="sunset_description"><?php print $description; ?></h2>
         <div class="icons_warpper">
 
         </div>
     </div>
 </div>
 
-<form method="POST" action="options.php">
+<form method="POST" action="options.php" class="sunset_general_form">
     <?php settings_fields('sunset-setting-group'); ?>
     <?php do_settings_sections('sunset_custom_option');?>
     <?php submit_button();?>
