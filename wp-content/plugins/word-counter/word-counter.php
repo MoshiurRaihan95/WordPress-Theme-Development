ffa846f68f1bfe7b04d7893c021b2ebaadce59f0
MoshiurRaihan95 <moshiurraihan95@gmail.com>
2022-10-11 23:27:15 +0600

    post word count plugin done

--- wp-content/plugins/word-counter/word-counter.php
+++ wp-content/plugins/word-counter/word-counter.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Plugin Name: Word Counter
 Plugin URI: 
@@ -12,29 +12,51 @@ Domain Path: /languages/
 */
 
 //Plugin Activation Hook
-function wordcount_activation_hook(){
-
+function wordcount_activation_hook()
+{
 }
-register_activation_hook(__File__,"wordcount_activation_hook");
+register_activation_hook(__File__, "wordcount_activation_hook");
 
 //Plugin Deactivation Hook
-function wordcount_deactivation_hook(){
-
+function wordcount_deactivation_hook()
+{
 }
-register_deactivation_hook(__File__,"wordcount_deactivation_hook");
+register_deactivation_hook(__File__, "wordcount_deactivation_hook");
 
 //Load Text Domain
-function wordcount_load_textdomain(){
-    load_plugin_textdomain('word-conter', false,dirname(__FILE__)."/languages");
+function wordcount_load_textdomain()
+{
+    load_plugin_textdomain('word-conter', false, dirname(__FILE__) . "/languages");
 }
-add_action("plugin_loaded",'wordcount_load_textdomain');
+add_action("plugin_loaded", 'wordcount_load_textdomain');
 
 
-function wordcount_count_words($content){
+function wordcount_count_words($content)
+{
     $stripped_content = strip_tags($content);
     $wordn = str_word_count($stripped_content);
     $label = __('Total Number Of Words', 'word-conter');
-    $content .= sprintf('<h6>%s: %s</h6>',$label,$wordn);
+    $label = apply_filters("wordcount_heading", $label);
+    $tag = apply_filters("wordcount_tag", 'h2', 'h2');
+    $content .= sprintf('<%s>%s: %s</%s>', $tag, $label, $wordn, $tag);
     return $content;
 }
-add_filter('the_content',"wordcount_count_words");
+add_filter('the_content', "wordcount_count_words");
+
+function wordcount_reading_time($content)
+{
+    $stripped_content = strip_tags($content);
+    $wordn = str_word_count($stripped_content);
+    $reding_minute = floor($wordn / 200);
+    $reding_seconds = floor($wordn % 200 / (200 / 60));
+    $is_visible = apply_filters('wordcount_display_readingtime', 1);
+    if ($is_visible) {
+        $label = __('Total Reading Time', 'word-conter');
+        $label = apply_filters("wordcount_readingtime_heading", $label);
+        $tag = apply_filters("wordcount_readingtime_tag", 'h4', 'h4');
+        $content .= sprintf('<%s>%s: %s minutes %s seconds</%s>', $tag, $label, $reding_minute, $reding_seconds, $wordn, $tag);
+    }
+
+    return $content;
+}
+add_filter('the_content', "wordcount_reading_time");
