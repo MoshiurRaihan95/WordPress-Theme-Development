73675551ceed0be05f1b84011ce06e48c061a257
MoshiurRaihan95 <moshiurraihan95@gmail.com>
2022-10-27 10:15:44 +0600

    first widget work done

--- wp-content/themes/twentytwenty/lib/first-widget.php
+++ wp-content/themes/twentytwenty/lib/first-widget.php
@@ -56,56 +56,133 @@ class FirstWidgets extends Widget_Base
             ]
         );
         //Image Content
+
         $this->add_control(
-            'image_content',
+            'image',
             [
-                'lable' => __('Image', 'custom-widget'),
-                'type' => Controls_Manager::MEDIA,
-                'dynamic' => [
-                    'active' => true,
-                ],
-                'label_block' => true,
-            ]
-        );
-        //Image Resize Control
-        $this->add_group_control(
-			Group_Control_Image_Size::get_type(),
-			[
-				'name' => 'thumbnail', // Usage: `{name}_size` and `{name}_custom_dimension`, in this case `thumbnail_size` and `thumbnail_custom_dimension`.
-				'exclude' => [ 'custom' ],
-				'include' => [],
-				'default' => 'large',
-			]
-		);
-        $this->add_responsive_control(
-            'img_alignment',
-            [
-                'label' => __('Alignment', 'custom_widget'),
-                'type' => Controls_Manager::CHOOSE,
-                'options' => [
-                    'left' => [
-                        'title' => __('Left', 'custom-widget'),
-                        'icon' => 'eicon-text-align-left',
-                    ],
-                    'center' => [
-                        'title' => __('Center', 'custom-widget'),
-                        'icon' => 'eicon-text-align-center',
-                    ],
-                    'right' => [
-                        'title' => __('Right', 'custom-widget'),
-                        'icon' => 'eicon-text-align-right',
-                    ],
-                ],
-                'devices' => [ 'desktop', 'tablet', 'mobile' ],
-                'img-alignment' => 'img_alignment-%s',
-                'selector' => [
-                    '{{WRAPPER}} img' => 'text-align: {{VALUE}};',
+                'label' => esc_html__('Choose Image', 'textdomain'),
+                'type' => \Elementor\Controls_Manager::MEDIA,
+                'default' => [
+                    'url' => \Elementor\Utils::get_placeholder_image_src(),
                 ],
             ]
         );
 
+        $this->add_group_control(
+            \Elementor\Group_Control_Image_Size::get_type(),
+            [
+                'name' => 'thumbnail', // Usage: `{name}_size` and `{name}_custom_dimension`, in this case `thumbnail_size` and `thumbnail_custom_dimension`.
+                'exclude' => ['custom'],
+                'include' => [],
+                'default' => 'large',
+            ]
+        );
+
+        $this->add_responsive_control(
+            'align',
+            [
+                'label' => esc_html__('Alignment', 'elementor'),
+                'type' => Controls_Manager::CHOOSE,
+                'options' => [
+                    'left' => [
+                        'title' => esc_html__('Left', 'elementor'),
+                        'icon' => 'eicon-text-align-left',
+                    ],
+                    'center' => [
+                        'title' => esc_html__('Center', 'elementor'),
+                        'icon' => 'eicon-text-align-center',
+                    ],
+                    'right' => [
+                        'title' => esc_html__('Right', 'elementor'),
+                        'icon' => 'eicon-text-align-right',
+                    ],
+                ],
+                'selectors' => [
+                    '{{WRAPPER}}' => 'text-align: {{VALUE}};',
+                ],
+            ]
+        );
+
+
+
+
+        // $this->add_control(
+        //     'image_content_up',
+        //     [
+        //         'lable' => __('Image', 'custom-widget'),
+        //         'type' => Controls_Manager::MEDIA,
+        //         'dynamic' => [
+        //             'active' => true,
+        //         ],
+        //         'label_block' => true,
+        //     ]
+        // );
+        // //Image Resize Control
+        // $this->add_group_control(
+        //     Group_Control_Image_Size::get_type(),
+        //     [
+        //         'name' => 'thumbnail', // Usage: `{name}_size` and `{name}_custom_dimension`, in this case `thumbnail_size` and `thumbnail_custom_dimension`.
+        //         'exclude' => ['custom'],
+        //         'include' => [],
+        //         'default' => 'large',
+        //     ]
+        // );
+        // $this->add_responsive_control(
+        //     'image_alignment',
+        //     [
+        //         'label' => __('Alignment', 'hip'),
+        //         'type' => Controls_Manager::CHOOSE,
+        //         'options' => [
+        //             'left' => [
+        //                 'title' => __('Left', 'hip'),
+        //                 'icon' => 'eicon-text-align-left',
+        //             ],
+        //             'center' => [
+        //                 'title' => __('Center', 'hip'),
+        //                 'icon' => 'eicon-text-align-center',
+        //             ],
+        //             'right' => [
+        //                 'title' => __('Right', 'hip'),
+        //                 'icon' => 'eicon-text-align-right',
+        //             ],
+        //         ],
+        //         'default' => 'center',
+        //         'selectors' => [
+        //             '{{WRAPPER}} img' => 'text-align: {{VALUE}}',
+        //         ],
+        //     ]
+        // );
+
         //end controls
         $this->end_controls_section();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         //Title
         $this->start_controls_section(
@@ -128,41 +205,42 @@ class FirstWidgets extends Widget_Base
                 'label_block' => true,
             ]
         );
-        
-        //add alignment control
+
+
+
         $this->add_responsive_control(
-            'title_alignment',
+            'back_text_alignment',
             [
-                'label' => __('Alignment', 'custom_widget'),
+                'label' => __('Alignment', 'hip'),
                 'type' => Controls_Manager::CHOOSE,
                 'options' => [
                     'left' => [
-                        'title' => __('Left', 'custom-widget'),
+                        'title' => __('Left', 'hip'),
                         'icon' => 'eicon-text-align-left',
                     ],
                     'center' => [
-                        'title' => __('Center', 'custom-widget'),
+                        'title' => __('Center', 'hip'),
                         'icon' => 'eicon-text-align-center',
                     ],
                     'right' => [
-                        'title' => __('Right', 'custom-widget'),
+                        'title' => __('Right', 'hip'),
                         'icon' => 'eicon-text-align-right',
                     ],
                 ],
-                'devices' => [ 'desktop', 'tablet', 'mobile' ],
-                'section_title' => 'title_alignment-%s',
-                'selector' => [
-                    '{{WRAPPER}} h4' => 'text-align: {{VALUE}};',
+                'default' => 'center',
+                'selectors' => [
+                    '{{WRAPPER}} h4' => 'text-align: {{VALUE}}',
                 ],
             ]
         );
+
         //end control
         $this->end_controls_section();
 
 
         //Content
         $this->start_controls_section(
-            'content_section',
+            'content_section_description',
             [
                 'label' => __('Content', 'custom-widget'),
                 'tab' => Controls_Manager::TAB_CONTENT,
@@ -184,28 +262,27 @@ class FirstWidgets extends Widget_Base
         );
         //add alignment control
         $this->add_responsive_control(
-            'description_alignment',
+            'description_align',
             [
-                'label' => __('Alignment', 'custom_widget'),
+                'label' => __('Alignment', 'hip'),
                 'type' => Controls_Manager::CHOOSE,
                 'options' => [
                     'left' => [
-                        'title' => __('Left', 'custom-widget'),
+                        'title' => __('Left', 'hip'),
                         'icon' => 'eicon-text-align-left',
                     ],
                     'center' => [
-                        'title' => __('Center', 'custom-widget'),
+                        'title' => __('Center', 'hip'),
                         'icon' => 'eicon-text-align-center',
                     ],
                     'right' => [
-                        'title' => __('Right', 'custom-widget'),
+                        'title' => __('Right', 'hip'),
                         'icon' => 'eicon-text-align-right',
                     ],
                 ],
-                'devices' => [ 'desktop', 'tablet', 'mobile' ],
-                'section_description' => 'description_alignment-%s',
-                'selector' => [
-                    '{{WRAPPER}} p' => 'text-align: {{VALUE}};',
+                'default' => 'left',
+                'selectors' => [
+                    '{{WRAPPER}} p' => 'text-align: {{VALUE}}',
                 ],
             ]
         );
@@ -221,7 +298,7 @@ class FirstWidgets extends Widget_Base
                 'tab' => Controls_Manager::TAB_STYLE,
             ]
         );
-        
+
         //add style control
         $this->add_control(
             'title_options',
@@ -252,23 +329,23 @@ class FirstWidgets extends Widget_Base
             ]
         );
         $this->add_group_control(
-			Group_Control_Border::get_type(),
-			[
-				'name' => 'border',
-				'selector' => '{{WRAPPER}} img',
-			]
-		);
-       //Image border radius contorl
+            Group_Control_Border::get_type(),
+            [
+                'name' => 'border',
+                'selector' => '{{WRAPPER}} img',
+            ]
+        );
+        //Image border radius contorl
         $this->add_responsive_control(
             'img_border_radius',
             [
                 'label' => __('Border Radius', 'custom-widget'),
                 'type' => Controls_Manager::DIMENSIONS,
-                'size_units' => [ 'px', '%', 'em' ],
+                'size_units' => ['px', '%', 'em'],
                 'selectors' => [
                     '{{WRAPPER}} img' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
                 ],
-                
+
             ]
         );
         //end style control;
@@ -278,11 +355,29 @@ class FirstWidgets extends Widget_Base
     protected function render()
     {
         $settings = $this->get_settings_for_display();
-        $img = !empty($settings['image_content']['url']) ? $settings['image_content']['url'] : '';
+        // $img = !empty($settings['image_content']['url']) ? $settings['image_content']['url'] : '';
+
+?>
+        <div class="my-img">
+            <style>
+                .my-img img {
+                    display: inline-block;
+                }
+            </style>
+            <?php
+
+            echo \Elementor\Group_Control_Image_Size::get_attachment_image_html($settings, 'thumbnail', 'image');
+
+            ?>
+        </div>
+<?php
+
+
+
         //echo Group_Control_Image_Size::get_attachment_image_html( $settings, 'thumbnail', '$img' );
-        
+
         // Get image url
-        echo '<img src="' . esc_url($img) . '" alt="">';
+        //echo '<img src="' . esc_url($img) . '" alt="">';
         echo '<h4 class="section_ttile">' . $settings['title_content'] . '</h4>';
         echo '<p  class="section_description">' . $settings['item_description'] . '</p>';
 
@@ -292,7 +387,7 @@ class FirstWidgets extends Widget_Base
 
     // protected function content_template()
     // {
-    
+
     // }
 }
 Plugin::instance()->widgets_manager->register_widget_type(new FirstWidgets);
